a174659f31ae55819494084777905b83191b59f1
Norven Koh Pei Ming <norven.koh.2024@computing.smu.edu.sg>
2025-10-07 04:50:50 +0800

    added Pantry
    still quite buggy, figuring out how to add conditionals

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "../pages/Login.vue";
 import SmartRecipes from "../pages/SmartRecipes.vue";
+import Pantry from "../pages/Pantry.vue";
 
 const routes = [
   { path: "/", redirect: "/login" },
   { path: "/login", component: Login },
   { path: "/recipe", component: SmartRecipes },
+  { path: "/pantry",component: Pantry},
 ];
 
 const router = createRouter({
