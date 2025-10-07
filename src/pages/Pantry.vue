f111f343054b33524558eaea7cd810c70865ce7a
Norven Koh Pei Ming <norven.koh.2024@computing.smu.edu.sg>
2025-10-08 05:20:18 +0800

    Update Pantry.vue
    temporary remove of other parts to add the createItem function, it works now :)

--- src/pages/Pantry.vue
+++ src/pages/Pantry.vue
@@ -1,77 +1,56 @@
 <style>
-    body {
-        
-    }
-    .fresh {
-        background-color: lightgreen;
-    }
-    .expiring {
-        background-color: lightyellow;
-    }
-    .expired {
-        background-color: lightcoral;
-    }
+body {
+
+}
+.fresh {
+    background-color: lightgreen;
+}
+.expiring {
+background-color: lightyellow;
+}
+.expired {
+background-color: lightcoral;}
 </style>
 
-<template>
 
-    
+<template>
 <div class="card p-3 mb-4 border border-1">
     <h5>New Item</h5>
-    <form id="pantryForm" class="row g-3">
-    <div class="col-6">
-        <input type="text" id="itemName" class="New_item" placeholder="Item name">
-    </div>
-<div class="dropdown">
-    <button class="btn btn-secondary dropdown-toggle d-block" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-    Food Category
-    </button>
-    <ul class="dropdown-menu">
-        <li><a class="dropdown-item" href="#">Fruits</a></li>
-        <li><a class="dropdown-item" href="#">Vegetables</a></li>
-        <li><a class="dropdown-item" href="#">Meats</a></li>
-    </ul>
-</div>
+    <form id="pantryForm" class="row g-3" @submit.prevent="createItem">
+        <div class="col-10">
+            <input type="text" id="itemName" class="New_item" placeholder="Item name" v-model="newItemText">
+        </div>
     <div class="col-2">
-        <input type="date" id="expiryDate" class="form-control">
-    </div>
-    <div class="col-md-2">
         <button type="submit" class="btn btn-primary w-100">Add</button>
+        </div>
+        </form>
     </div>
-    </form>
-</div>
 
-<h3>Current Pantry</h3>
-<div id="itemList">
+    <h3>Current Pantry</h3>
+    <div id="itemList">
     <ul>
-        <li>
-            <div v-for="item in items" :key="index">
-                {{ item.text }}
-            </div>
-        </li>
+    <li v-for="(item, index) in items" :key="index">
+        {{ item }}
+    </li>
     </ul>
-    
-</div>
-
+    </div>
 </template>
 
 <script>
-
-    export default {
-        itemList() {
-            return {
-                fruits: [],
-                vegetable: [],
-                meat: [],
-                items: ["apple"]
-            }
-        },
-
+export default {
+    data() {
+        return {
+        newItemText: '',
+        items: []
+    };
+    },
     methods: {
-        createItem() {
-
+    createItem() {
+        if (this.newItemText.trim() !== '') {
+        this.items.push(this.newItemText.trim());
+        this.newItemText = '';
+    }
         }
     }
-    }
-
+};
 </script>
