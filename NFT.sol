ced3078c47974409eaff31ec12c3dff5b19aa563
Jun Wei <53926665+0xjunwei@users.noreply.github.com>
2025-10-17 14:19:26 +0800

    ERC721 Contract for badges

---
+++ NFT.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.4.0
+pragma solidity ^0.8.27;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract MyToken is ERC721, ERC721URIStorage, Ownable {
+    uint256 private _nextTokenId;
+
+    constructor()
+        ERC721("MyToken", "MTK")
+        Ownable(msg.sender)
+    {}
+
+    // Image and Json format, Metamask requires the image to be stored in a json format to have a description
+    // Image at : https://storage.googleapis.com/meal-planner-badges/sustainable%20badge.png#
+    // Json at: https://storage.googleapis.com/meal-planner-badges/1.json#
+    function safeMint(address to, string memory uri)
+        public
+        onlyOwner
+        returns (uint256)
+    {
+        uint256 tokenId = _nextTokenId++;
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+        return tokenId;
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
