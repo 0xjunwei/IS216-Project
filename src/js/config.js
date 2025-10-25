dd7a6220354250242201213c2734ccd4a4aae5d7
Jun Wei <53926665+0xjunwei@users.noreply.github.com>
2025-10-25 12:07:18 +0800

    Update config.js
    Mate

--- src/js/config.js
+++ src/js/config.js
@@ -6,6 +6,7 @@ import { getAnalytics } from "firebase/analytics";
 
 // Firebase configuration from Jun Wei
 const firebaseConfig = {
+<<<<<<< Updated upstream
   apiKey: "AIzaSyDh0v1iR1zHq1U0P-BEQXHiYEsys0o3nMI",
   authDomain: "meal-planner-b7e64.firebaseapp.com",
   databaseURL: "https://meal-planner-b7e64-default-rtdb.firebaseio.com",
@@ -14,6 +15,16 @@ const firebaseConfig = {
   messagingSenderId: "81058674749",
   appId: "1:81058674749:web:8a0df7eda06da992081254",
   measurementId: "G-TH15G7VKN0"
+=======
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+>>>>>>> Stashed changes
 };
 
 // Initialize Firebase app
